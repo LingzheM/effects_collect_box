15f063f2ae8fe66d82a4f7d0cea40e026fc60852
mou <mou.reitetsu@gmail.com>
2026-05-30 23:47:11 +0900

    add energy charge

--- src/app/gsap/herobase/EnergyBar.tsx
+++ src/app/gsap/herobase/EnergyBar.tsx
@@ -1,5 +1,6 @@
 import { useRef } from "react";
 import styles from './HeroBase.module.css';
+import gsap from "gsap";
 
 interface EnergyBarProps {
   /** 充能时长 */
@@ -9,11 +10,23 @@ interface EnergyBarProps {
 export function EnergyBar({ chargeDuration = 1.5}: EnergyBarProps) {
   const barRef = useRef<HTMLDivElement>(null);
 
+  // 充能：fromTo 明确指定 0% -> 100%
+  const charge = () => {
+    gsap.fromTo(
+      barRef.current,
+      { width: '0%' },  // 起点
+      { width: '100%', duration: chargeDuration, ease: 'power2.out' } // 终点
+    );
+  };
+
   return (
     <>
       <div className={styles.barContainer}>
         <div ref={barRef} className={styles.bar} />
       </div>
+      <button className={styles.btn} onClick={charge}>
+        激活蓄能（fromTo）
+      </button>
     </>
   )
 }
